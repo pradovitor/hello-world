b58b7886af10fc674bcf82a66d3ffb9f42e70e7f
Prado <vitorprd@hotmail.com>
2023-12-12 18:14:42 -0300

    Adiciona projeto Hello World

---
+++ hello world.c
@@ -0,0 +1,13 @@
+//as linhas chamam as bibliotecas 
+//contém funções definidas para usarmos 
+//a primeira define entrada e saída e a segunda o básico do sistema 
+#include <stdio.h>
+#include <stdlib.h>
+
+//main é o ponto de partida, com argumentos e parametros argc numero de linhas argv o conteúdo e o char é um tipo de variável
+int main(int argc, char *argv[]) 
+{
+	printf("\n Hello World \n");
+	system("pause");
+	return 0;
+}
